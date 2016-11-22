235a47857c8790d5ded32fd4612e453250a5e936
zeusofohio <zeusofohio@gmail.com>
2016-11-21 20:44:13 -0500

    Updated

---
+++ AccessingStrings.py
@@ -0,0 +1,17 @@
+'''
+ Author      : DORIAN JAVA BROWN
+ Version     : N/A
+ Copyright   : All Rights Reserve; You may use, distribute and modify this code.
+ Description : This program accessing Values in Strings
+ '''
+
+var1 = 'Hello World!'
+var2 = "Python Programming"
+
+print ""
+print "var1[0]: ", var1[0:5]
+print "var2[0:6]: ", var2[0:6]
+print ""
+
+print var1[0:5] + " " + var2[0:6]
+print ""
