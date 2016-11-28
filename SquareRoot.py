147dcc8085370deaf463d2109781733c40f994c4
zeusofohio <zeusofohio@gmail.com>
2016-11-28 14:27:58 -0500

    Updated

---
+++ SquareRoot.py
@@ -0,0 +1,16 @@
+'''
+ Author      : DORIAN JAVA BROWN
+ Version     : N/A
+ Copyright   : All Rights Reserve; You may use, distribute and modify this code.
+ Description : This program calculates the square root
+ '''
+
+# input data from the user
+num = float(input('enter a number: '))
+
+# square root calculations
+num_sqrt = num ** 0.5
+
+# user results displayed
+print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
+ 
