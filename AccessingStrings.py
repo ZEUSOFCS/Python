8aecaf0119770ded8e3dcf3859762e4ac2a8fd3c
zeusofohio <zeusofohio@gmail.com>
2016-11-28 20:57:55 -0500

    Updated SQ RT

--- AccessingStrings.py
+++ AccessingStrings.py
@@ -14,7 +14,5 @@ print "\n var2[0:6]: ", var2[0:6]
 
 <<<<<<< HEAD
 print "\n\t\t" + var1[0:5] + " " + var2[0:6], "\n"
-=======
 print var1[0:5] + " " + var2[0:6]
 print ""
->>>>>>> 0e13a40df593a8d8fab018b4e40a3665c7080c45
