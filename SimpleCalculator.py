20f4a000bd16272db1b385bf513cbb6958711089
zeusofohio <zeusofohio@gmail.com>
2016-12-16 00:52:43 -0500

    Update Fix

--- SimpleCalculator.py
+++ SimpleCalculator.py
@@ -9,54 +9,74 @@
 
 '''functions definitions'''
 
-# addition function
-def add(x, y):
-   return x + y
-
-# subtraction function
-def subtract(x, y):
-   return x - y
-
-# multiplication function
-def multiply(x, y):
-   return x * y
-
-# divison function
-def divide(x, y):
-   return x / y
-
-# operation selection from the user
-print("\t\tSelect operation:")
-print("==============================================")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
 
 
-print('Please type in the math operation you would like to complete:')
-print('-------------------------------------------------------------')
-print('+ for addition')
-print('- for subtraction')
-print('* for multiplication')
-print('/ for division')
-print('\n\n')
 
-choice = input("Enter choice(1/2/3/4):")
 
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
 
-if choice == '1':
-   print(num1,"+",num2,"=", add(num1,num2))
 
-elif choice == '2':
-   print(num1,"-",num2,"=", subtract(num1,num2))
 
-elif choice == '3':
-   print(num1,"*",num2,"=", multiply(num1,num2))
 
-elif choice == '4':
-   print(num1,"/",num2,"=", divide(num1,num2))
-else:
-   print("Invalid input")
+
+
+
+
+
+
+
+
+
+def calculate():
+
+  # operation selection from the user
+  print('Please type in the math operation you would like to complete:')
+  print('-------------------------------------------------------------')
+  print('+ for addition')
+  print('- for subtraction')
+  print('* for multiplication')
+  print('/ for division')
+  print('\n\n')
+
+  operation = input('Enter Operation symbol: ')
+
+  num1 = int(input("Enter first number: "))
+  num2 = int(input("Enter second number: "))
+
+  if operation == ' ' :
+    
+  elif operation == ' ' :
+  
+  elif operation == ' ' :
+    
+  elif operation == ' ' :
+  
+  elif operation == ' ' :
+    
+  else :
+  
+  # addition
+
+  # subtraction
+
+  # multiplication 
+
+  # divison
+
+
+
+  choice = input("Enter choice(1/2/3/4):")
+
+  
+  if choice == '1':
+    print(num1,"+",num2,"=", add(num1,num2))
+
+    elif choice == '2':
+      print(num1,"-",num2,"=", subtract(num1,num2))
+
+      elif choice == '3':
+        print(num1,"*",num2,"=", multiply(num1,num2))
+
+        elif choice == '4':
+          print(num1,"/",num2,"=", divide(num1,num2))
+          else:
+            print("Invalid input")
