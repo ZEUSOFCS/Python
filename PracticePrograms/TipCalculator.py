cf0fb8dfeeccfaf42c8e433a9f7826ec77febabd
zeusofohio <zeusofohio@gmail.com>
2017-01-09 16:38:23 -0500

    Updated

--- PracticePrograms/TipCalculator.py
+++ PracticePrograms/TipCalculator.py
@@ -2,8 +2,13 @@
  Author      : DORIAN JAVA BROWN
  Version     : N/A
  Copyright   : All Rights Reserve; You may use, distribute and modify this code.
- Description :  
+ Description : This program provides the user with options on how much tip the customer should leave the waiter/waitress
  '''
 
-Tip CAlC
+# menu
+
+print('\n\n JUICEY BURGER')
+print('Bill Total:   $19.99')
+print("\n\n")
+
 tip = 0.00
