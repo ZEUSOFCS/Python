16ebd969fd18f5578780432e9d23532283e93ebd
zeusofohio <zeusofohio@gmail.com>
2017-01-18 23:05:08 -0500

    Update Menu

--- PracticePrograms/TipCalculator.py
+++ PracticePrograms/TipCalculator.py
@@ -10,9 +10,36 @@ import os
 total = 21.49
 
 def cls():
-  os.system('cls' if os.name=='nt' else 'clear')
-# menu
+  os.system('cls' if os.name == 'nt' else 'clear')
+  
+def opt():        #opt abbreviation 
+    
+    
+  answer = raw_input('Correct ?  ')
 
+  if answer == 'YES' or answer == 'Yes' or answer == 'yes' :
+  cls()
+  
+  # tip suggestion list
+  print('\n\n\t Tip Suggestions')
+  print('----------------------------------')
+  print('A)  %%20           $ %0.3f' %((total * .20)))
+  print('B)  %%15           $ %0.3f' %((total * .15)))
+  print('C)  %%10           $ %0.3f' %((total * .10)))
+  print('D)   %%5           $ %0.3f' %((total * .05)))
+  print('E)  No Thank You /n/n')
+  
+  print
+  
+elif answer == 'NO' or answer == 'No' or answer == 'no' :
+  print ('\n\n\t\t please wait one moment for assistance...\n\n')
+  
+else:
+      print('\n\n\t\t error:. invaild value \n\n')
+
+def menu():
+  
+# menu display
 print('\n\n\t\t JUICEY BURGER\n\n')
 print('')
 print('1 Juicey Burger  $ 18.99')
@@ -24,25 +51,9 @@ print('Local Tax:       $  1.50')
 print('Bill Total:      $ 21.49')
 print('\n\n')
 
-answer = raw_input('Correct ?  ')
-
-if answer == 'YES' or answer == 'Yes' or answer == 'yes' :
-  cls()
-  
-  # tip suggestion list
-  print('\n\n\t Tip Suggestiozzns')
-  print('----------------------------------')
-  print('A)  %%20           $ %0.3f' %((total * .20)))
-  print('B)  %%15           $ %0.3f' %((total * .15)))
-  print('C)  %%10           $ %0.3f' %((total * .10)))
-  print('D)   %%5           $ %0.3f' %((total * .05)))
-  
-  print
-  
-elif answer == 'NO' or answer == 'No' or answer == 'no' :
-  print ('\n\n\t\t please wait one moment for assistance...\n\n')
-  
-else:
-      print('\n\n\t\t error:. invaild value \n\n')
 
 #https://www.youtube.com/watch?annotation_id=annotation_3770292585&feature=iv&src_vid=bguKhMnvmb8&v=LtGEp9c6Z-U
+
+
+menu()
+opt()
